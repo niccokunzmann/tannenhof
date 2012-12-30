e20ae759487c82d0af0d8a0ec2da68590464559c
Nicco Kunzmann <niccokunzmann@googlemail.com>
2012-12-30 17:42:43 +0100

    nun werden die webseiten erstmal richtig generiert

--- website/generate_website.py
+++ website/generate_website.py
@@ -20,7 +20,7 @@ inputDir = '.'
 
 def include(filename):
     print ('including ' + str(filename))
-    return open(filename).read()
+    return open(os.path.join(inputDir, filename)).read()
 
 def languageName(fileName, lang = None):
     if lang is None:
@@ -33,11 +33,11 @@ if not os.path.isdir(outputDir):
 
 # delete output dir
 
-for dirPath, dirNames, fileNames in os.walk(outputDir):
+for dirPath, dirNames, fileNames in os.walk(outputDir, topdown = False):
     for fileName in fileNames:
-        os.remove(fileName)
+        os.remove(os.path.join(dirPath, fileName))
     for dirName in dirNames:
-        os.remove(dirName)
+        os.rmdir(os.path.join(dirPath, dirName))
 
 # generate new output
 
@@ -46,33 +46,39 @@ pythonRegex = re.compile('^(?P<start>.*?)\\{\\{\\{(?P<python>.*?)\\}\\}\\}(?P<en
 for language in ['de', 'en', 'ru']:
 
     for dirPath, dirNames, fileNames in os.walk(inputDir):
+        relativeOutputDir = outputDir + '/' + dirPath[len(inputDir):]
+        if not os.path.isdir(dirPath):
+            os.makedirs(dirPath)
+        for dirName in dirNames:
+            newDir = os.path.join(relativeOutputDir, dirName)
+            if not os.path.isdir(newDir):
+                os.makedirs(newDir)
+        for fileName in fileNames:
+            adaptFile = fileName.endswith('.t')
+            filePath = os.path.join(dirPath, fileName)
+            if adaptFile:
+                newFilePath = os.path.join(relativeOutputDir,\
+                                           languageName(fileName[:-2]))
+                print('from: %s \n\t%s' % (filePath, newFilePath))
+            else:
+                newFilePath = os.path.join(relativeOutputDir, fileName)
+            with open(filePath) as f:
+                content = f.read()
+            # replace content
+            while adaptFile:
+                m = pythonRegex.search(content)
+                if not m: break
+                s = m.group('python')
+                try:
+                    result = eval(s)
+                except:
+                    print '-' * 40
+                    print s
+                    time.sleep(5)
+                    raise
+                content = m.group('start') + str(result) + m.group('end')
+            with open(newFilePath, 'w') as f:
+                f.write(content)
         
-        if not fileName.endswith('.t'):
-            continue
-        filePath = os.path.join(inputDir, fileName)
-        newFilePath = os.path.join(outputDir, languageName(fileName[:-2]))
-        print('from: %s \n\t%s' % (filePath, newFilePath))
-        with open(filePath) as f:
-            content = f.read()
-        # replace content
-        while 1:
-            m = pythonRegex.search(content)
-            if not m: break
-            s = m.group('python')
-            try:
-                print 'eval'
-                result = eval(s)
-            except:
-                print '-' * 40
-                print s
-                time.sleep(5)
-                raise
-            content = m.group('start') + str(result) + m.group('end')
-                
-            
-            
-        with open(newFilePath, 'w') as f:
-            f.write(content)
-    
 
 
